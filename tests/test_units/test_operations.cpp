c4b6da9955d9f2d93c0de789b388077a597f078c
jgsogo <jgsogo@gmail.com>
2022-01-23 11:56:17 +0100

    [feat] NamedUnits: Add ctor from parent

---
+++ tests/test_units/test_operations.cpp
@@ -0,0 +1,17 @@
+
+#include <sstream>
+#include "catch2/catch.hpp"
+#include "units/milimeters.hpp"
+
+using namespace math::units;
+
+/*
+ * Units in this project should be able to perform some basics operations to work with other
+ * structures and template classses
+ * */
+
+TEST_CASE("test_units/test_operations | Assignments", "[units/milimeters]") {
+    math::types::MilimetersT<float> result = 10_mm - 23_mm;
+    REQUIRE(result == -13_mm);
+}
+
