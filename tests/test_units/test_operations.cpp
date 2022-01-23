0f1be4a7faf9bcf1704c157fec58a9bd81efd936
jgsogo <jgsogo@gmail.com>
2022-01-23 16:44:50 +0100

    add comment

--- tests/test_units/test_operations.cpp
+++ tests/test_units/test_operations.cpp
@@ -10,7 +10,7 @@ using namespace math::units;
  * structures and template classses
  * */
 
-TEST_CASE("test_units/test_operations | Assignments", "[units/milimeters]") {
+TEST_CASE("test_units/test_operations | Assignments from return type", "[units/milimeters]") {
     math::types::MilimetersT<float> result = 10_mm - 23_mm;
     REQUIRE(result == -13_mm);
 }
